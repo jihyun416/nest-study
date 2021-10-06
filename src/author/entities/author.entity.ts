7ef4c2ce35b5e987ee7165f1b112cd2fac12bb68
jihyun416 <songsjhsjh@gmail.com>
2021-10-06 16:56:28 +0900

    database

--- src/author/entities/author.entity.ts
+++ src/author/entities/author.entity.ts
@@ -1,1 +1,13 @@
-export class Author {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Author {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
