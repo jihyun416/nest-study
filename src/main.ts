c9e32c37f3f55a66c1f5c48fc2aca426512e4b2a
jihyun416 <songsjhsjh@gmail.com>
2021-10-15 17:40:45 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AllExceptionsFilter } from './exception/all-exception.filter';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
+  app.enableCors();
   app.use(logger);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
