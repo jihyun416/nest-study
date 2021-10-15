a9d254bd68f9ad4a48918021da0cae02536106f3
jihyun416 <songsjhsjh@gmail.com>
2021-10-15 17:47:23 +0900

    csurf

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { logger } from './logger.middleware';
 import { AllExceptionsFilter } from './exception/all-exception.filter';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.enableCors();
   app.use(logger);
+  app.use(csurf());
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
