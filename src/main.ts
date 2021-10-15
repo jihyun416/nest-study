ab1150abba8c469604941e73ca037a25e76f2fc2
jihyun416 <songsjhsjh@gmail.com>
2021-10-15 17:37:30 +0900

    helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './logger.middleware';
 import { AllExceptionsFilter } from './exception/all-exception.filter';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.use(logger);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
